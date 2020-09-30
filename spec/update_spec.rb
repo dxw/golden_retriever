f14eacf88171211f54728f4f26f0823e952a1a0d
Stuart Harrison <pezholio@gmail.com>
2020-09-30 14:43:43 +0100

    Tweak update tests
    The layout of the page has changed now. Rather than re-record HTTP requests,
    I've changed the tests to stub the expected responses, which makes for more
    readable specs

--- spec/update_spec.rb
+++ spec/update_spec.rb
@@ -7,48 +7,50 @@ RSpec.describe GoldenRetriever::Update, :vcr do
     allow(MarketplaceOpportunityScraper::Opportunity).to receive(:all) { [opportunity] }
   end
 
+  let(:opportunity) { double("MarketplaceOpportunityScraper::Opportunity", id: 12345) }
+
   context "when an opportunity has been awarded" do
-    let(:opportunity) { MarketplaceOpportunityScraper::Opportunity.find(9115) }
+    before do
+      allow(opportunity).to receive(:awarded_to) { "ACME Inc" }
+    end
 
     it "updates the successful_bidder" do
       deal = double("GoldenRetriever::Deal")
       expect(GoldenRetriever::Deal).to receive(:find_by_marketplace_id).with(opportunity.id) { deal }
       expect(deal).to receive(:successful_bidder) { nil }
-      expect(deal).to receive("successful_bidder=").with(opportunity.awarded_to)
+      expect(deal).to receive("successful_bidder=").with("ACME Inc")
       expect(deal).to receive(:save)
 
       GoldenRetriever::Update.new.run!
     end
+
+    context "when the deal does not exist" do
+      it "does not error" do
+        expect(GoldenRetriever::Deal).to receive(:find_by_marketplace_id).with(opportunity.id) { nil }
+
+        GoldenRetriever::Update.new.run!
+      end
+    end
+
+    context "when the deal already has a successful_bidder set" do
+      it "does not set it again" do
+        deal = double("GoldenRetriever::Deal")
+        expect(GoldenRetriever::Deal).to receive(:find_by_marketplace_id).with(opportunity.id) { deal }
+        expect(deal).to receive(:successful_bidder) { "ACME Inc" }
+        expect(deal).to_not receive(:save)
+
+        GoldenRetriever::Update.new.run!
+      end
+    end
   end
 
   context "when an opportunity has not been awarded" do
-    let(:opportunity) { MarketplaceOpportunityScraper::Opportunity.find(9482) }
+    before do
+      allow(opportunity).to receive(:awarded_to) { nil }
+    end
 
     it "does not fetch the deal" do
       expect(GoldenRetriever::Deal).to_not receive(:find_by_marketplace_id)
-
-      GoldenRetriever::Update.new.run!
-    end
-  end
-
-  context "when the deal does not exist" do
-    let(:opportunity) { MarketplaceOpportunityScraper::Opportunity.find(9115) }
-
-    it "does not error" do
-      expect(GoldenRetriever::Deal).to receive(:find_by_marketplace_id).with(opportunity.id) { nil }
-
-      GoldenRetriever::Update.new.run!
-    end
-  end
-
-  context "when the deal already has a successful_bidder set" do
-    let(:opportunity) { MarketplaceOpportunityScraper::Opportunity.find(9115) }
-
-    it "does not set it again" do
-      deal = double("GoldenRetriever::Deal")
-      expect(GoldenRetriever::Deal).to receive(:find_by_marketplace_id).with(opportunity.id) { deal }
-      expect(deal).to receive(:successful_bidder) { "ACME Inc" }
-      expect(deal).to_not receive(:save)
 
       GoldenRetriever::Update.new.run!
     end
